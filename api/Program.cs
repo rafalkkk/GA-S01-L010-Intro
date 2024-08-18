9752235bcc8f445d01d0b18ef906bb5ed105630e
rafalkkk <rafal.kraik@gmail.com>
2024-08-18 15:36:01 +0200

    Revert "broken"
    This reverts commit e317c96bfe924b8bd7bfc31e01148891bafd69ba.

--- api/Program.cs
+++ api/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.BuildError();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
